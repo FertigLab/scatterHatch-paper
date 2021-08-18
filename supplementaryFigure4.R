9ee9cb1a9ee534490461fa7ee98be48cada90e1e
roofTiles <guhatejas@gmail.com>
2021-08-18 00:05:06 -0400

    creates supplementary figures 4 and 5

---
+++ supplementaryFigure4.R
@@ -0,0 +1,31 @@
+library(scatterHatch)
+library(ggplot2)
+
+# Supplementary Figure 4
+
+pdacFrameDir = "C:\\umd\\scatterHatch\\figuresPaper\\supplementaryFigures\\suppFig3\\"
+
+pdacData = scatterHatch::pdacData
+pdacData$cellID = paste0('cell_', 1:nrow(pdacData))
+pdacData$Yt <- -pdacData$Yt
+pancreas_frames = c(1:6, 27:31, 15:19, 40:44)
+PDAC_frames = c(23:26, 35:37, 51:52, 64:65, 77)
+small_intestines_frames = c(49:50, 63, 75:76, 88:89, 100:103, 112:116, 125:129, 137:140)
+annotateLocation <- function(frame){
+    if (frame %in% pancreas_frames){return("Pancreas")}
+    if (frame %in% PDAC_frames){return("PDAC")}
+    if (frame %in% small_intestines_frames){return("Small Intestine")}
+    return("Other")
+}
+pdacData$location = sapply(pdacData$frame, annotateLocation)
+
+
+w = 4
+h = 3
+dev.new(width = w, height = h, noRStudioGD = TRUE)
+patternList = list(list(pattern = "\\", density=1/4), list(pattern = "-", density = 1/4), list(pattern = "/"), list(pattern = ""))
+supplementaryFigure4 = scatterHatch(data = pdacData, x = "Xt", y = "Yt", factor = "location", legendTitle = "Tissue Type", patternList = patternList) +
+    theme_void()
+plot(supplementaryFigure4)
+ggsave("C:\\umd\\scatterHatch\\figuresPaper\\supplementaryFigures\\Supplementary_Figure4.png", width=w, height=h, dpi = 350, bg="White")
+dev.off()
---
+++ supplementaryFigure5.R
@@ -0,0 +1,31 @@
+library(scatterHatch)
+library(ggplot2)
+
+# Supplementary Figure 5
+
+pdacFrameDir = "C:\\umd\\scatterHatch\\figuresPaper\\supplementaryFigures\\suppFig3\\"
+
+pdacData = scatterHatch::pdacData
+pdacData$cellID = paste0('cell_', 1:nrow(pdacData))
+pdacData$Yt <- -pdacData$Yt
+pancreas_frames = c(1:6, 27:31, 15:19, 40:44)
+PDAC_frames = c(23:26, 35:37, 51:52, 64:65, 77)
+small_intestines_frames = c(49:50, 63, 75:76, 88:89, 100:103, 112:116, 125:129, 137:140)
+annotateLocation <- function(frame){
+    if (frame %in% pancreas_frames){return("Pancreas")}
+    if (frame %in% PDAC_frames){return("PDAC")}
+    if (frame %in% small_intestines_frames){return("Small Intestine")}
+    return("Other")
+}
+pdacData$location = sapply(pdacData$frame, annotateLocation)
+
+
+w = 4
+h = 3
+dev.new(width = w, height = h, noRStudioGD = TRUE)
+patternList = list(list(pattern = "\\", lineColor = "white", density=1/5, lineType = "solid"), list(pattern = "x", density = 1/4, angle = c(45, 135, 90), lineWidth = 0.2, lineType = "solid", lineColor = "black"), list(pattern = "\\", lineWidth = 0.35, lineType = "dashed", lineColor = "black"), list(pattern = ""))
+supplementaryFigure5 = scatterHatch(data = pdacData, x = "Xt", y = "Yt", factor = "location", legendTitle = "Tissue Type", patternList = patternList) +
+    theme_void()
+plot(supplementaryFigure5)
+ggsave("C:\\umd\\scatterHatch\\figuresPaper\\supplementaryFigures\\Supplementary_Figure5.png", width=w, height=h, dpi = 350, bg="White")
+dev.off()
