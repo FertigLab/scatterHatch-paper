22ca1a8a19065163bb065190b4d337287d35c58c
roofTiles <guhatejas@gmail.com>
2021-09-06 20:53:07 -0400

    updates to umapFigure

--- umapFigure.R
+++ umapFigure.R
@@ -37,7 +37,7 @@ dev.new(width=w, height=h, noRStudioGD = TRUE)
 lineWidth <- 0.15
 patternList <- list(list(pattern="\\", lineWidth=lineWidth*1.5), list(pattern="x", lineWidth=lineWidth), list(pattern="-", lineWidth=lineWidth, density=1/4), list(pattern=""))
 colorPalette <- c("#009E73", "#E69F00", "#F0E442", "#56B4E9")
-plt <- scatterHatch(umapCoords, "x", "y", factor = "cluster", legendTitle = "Cluster", pointSize=1, patternList = patternList, colorPalette = colorPalette)
+plt <- scatterHatch(umapCoords, "x", "y", color_by = "cluster", legendTitle = "Cluster", pointSize=1, patternList = patternList, colorPalette = colorPalette)
 plt <- plt + theme_void() + labs(y= "", x = "") + theme(legend.position = "none")
 titles <- c("A", "B", "C", "D")
 perceptions <- c("origin", "deuteranope", "protanope", "desaturate")
@@ -60,7 +60,7 @@ patternList <- list(list(pattern="\\", lineWidth=lineWidth), list(pattern="x", l
 perceptions <- c("origin", "deuteranope", "protanope", "desaturate")
 
 dev.new(width = w, height = h, noRStudioGD = TRUE)
-plt <- scatterHatch(data = bounds, x="x", y="y", factor="cluster", pointSize=3, patternList = patternList, 
+plt <- scatterHatch(data = bounds, x="x", y="y", color_by="cluster", pointSize=3, patternList = patternList, 
                     colorPalette = colorPalette) + theme_void() + labs(y= "", x = "")
 
 i <- 1
@@ -76,35 +76,58 @@ for (i in seq(4)){
 ## Stitching UMAPs with zoom-in figures
 
 origin <- readTIFF(paste0(filePath, "umaps\\origin.tiff"))
-originZoom <- readTIFF(paste0(filePath, "zoomIn\\origin.tiff"))
+originLeg <- readTIFF(paste0(filePath, "zoomIn\\origin.tiff"))
 deu <- readTIFF(paste0(filePath, "umaps\\deuteranope.tiff"))
-deuZoom <- readTIFF(paste0(filePath, "zoomIn\\deuteranope.tiff"))
+deuLeg <- readTIFF(paste0(filePath, "zoomIn\\deuteranope.tiff"))
 pro <- readTIFF(paste0(filePath, "umaps\\protanope.tiff"))
-proZoom <- readTIFF(paste0(filePath, "zoomIn\\protanope.tiff"))
+proLeg <- readTIFF(paste0(filePath, "zoomIn\\protanope.tiff"))
 des <- readTIFF(paste0(filePath, "umaps\\desaturate.tiff"))
-desZoom <- readTIFF(paste0(filePath, "zoomIn\\desaturate.tiff"))
+desLeg<- readTIFF(paste0(filePath, "zoomIn\\desaturate.tiff"))
 
 par(mar=rep(0,4))
-outline = matrix(c(rep(c(1), times = 3), rep(2, times = 2), rep(3, times = 3), rep(4, times = 2),
-                   rep(5, times = 3), rep(6, times = 2), rep(7, times = 3), rep(8, times = 2)), 
+outline = matrix(c(rep(c(1), times = 3), rep(2, times = 1), rep(3, times = 1), rep(4, times = 3), rep(5, times = 1),
+                   rep(6, times = 1), rep(7, times = 3), rep(8, times = 1), rep(9, times = 1), rep(10, times = 3), rep(11, times = 1), rep(12, times = 1)), 
                  ncol = 10, nrow = 2, byrow = TRUE)
 
 layout(outline)
+zoomRows = 1350:2100
+zoomCols = 1850:2350
+zoomL = 0
+zoomB = 0.2
+zoomR = 1
+zoomT = 0.8
+
+legRows = 200:3400
+legCols = 1750:2400
+legL = 0.2
+legB = 0
+legR = 0.8
+legT = 1
+
 plot(NA,xlim=0:1,ylim=0:1,xaxt="n",yaxt="n",bty="n")
 rasterImage(origin,0,0,1,1)
 plot(NA,xlim=0:1,ylim=0:1,xaxt="n",yaxt="n",bty="n")
-rasterImage(originZoom,0,0,1,1)
+rasterImage(origin[zoomRows,zoomCols,],zoomL,zoomB,zoomR,zoomT)
+plot(NA,xlim=0:1,ylim=0:1,xaxt="n",yaxt="n",bty="n")
+rasterImage(originLeg[legRows,legCols,],legL,legB,legR,legT)
 plot(NA,xlim=0:1,ylim=0:1,xaxt="n",yaxt="n",bty="n")
 rasterImage(deu,0,0,1,1)
 plot(NA,xlim=0:1,ylim=0:1,xaxt="n",yaxt="n",bty="n")
-rasterImage(deuZoom,0,0,1,1)
+rasterImage(deu[zoomRows,zoomCols,],zoomL,zoomB,zoomR,zoomT)
+plot(NA,xlim=0:1,ylim=0:1,xaxt="n",yaxt="n",bty="n")
+rasterImage(deuLeg[legRows,legCols,],legL,legB,legR,legT)
 plot(NA,xlim=0:1,ylim=0:1,xaxt="n",yaxt="n",bty="n")
 rasterImage(pro,0,0,1,1)
 plot(NA,xlim=0:1,ylim=0:1,xaxt="n",yaxt="n",bty="n")
-rasterImage(proZoom,0,0,1,1)
+rasterImage(pro[zoomRows,zoomCols,],zoomL,zoomB,zoomR,zoomT)
+plot(NA,xlim=0:1,ylim=0:1,xaxt="n",yaxt="n",bty="n")
+rasterImage(proLeg[legRows,legCols,],legL,legB,legR,legT)
 plot(NA,xlim=0:1,ylim=0:1,xaxt="n",yaxt="n",bty="n")
 rasterImage(des,0,0,1,1)
 plot(NA,xlim=0:1,ylim=0:1,xaxt="n",yaxt="n",bty="n")
-rasterImage(desZoom,0,0,1,1)
+rasterImage(des[zoomRows,zoomCols,],zoomL,zoomB,zoomR,zoomT)
+plot(NA,xlim=0:1,ylim=0:1,xaxt="n",yaxt="n",bty="n")
+rasterImage(desLeg[legRows,legCols,],legL,legB,legR,legT)
+
 dev.print(tiff, paste0(filePath, "umap.tiff"), width=5, height=3, units="in", res=1200)
 dev.print(tiff, paste0(filePath, "umapLowRes.tiff"), width=5, height=3, units="in", res=350)
